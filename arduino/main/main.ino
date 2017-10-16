689d8d0f2e5ffbe4c564dfb51bb15d4e48ac4a67
Michael Osei <mosei27@live.com>
2017-10-16 18:47:35 -0500

    Fix gyro readings

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,14 +1,31 @@
 #include "Arduino.h"
 
 #include "gyro.h"
+//#include "PinChangeInterrupt.h"
 
 Gyroscope* gyro_ptr;
 
 void setup() {
+  //pinMode(INTERRUPT_PIN, INPUT);
+  Serial.begin(9600);
+  Serial.println("Setting up gyro...");
 	gyro_ptr =  new Gyroscope;
+  Serial.println("Done! setting up gyro...");
+  // attach new pinchange interrupt
+  //attachPCINT(digitalPinToPCINT(2), readGyro, CHANGE);
+}
+
+void readGyro() {
+  Gyroscope::Values values = gyro_ptr->read();
+  Serial.println(String(values.ax) + " ax, "
+                 + String(values.ay) + " ay, "
+                 + String(values.az) + " az, "
+                 + String(values.gx) + " gx, "
+                 + String(values.gy) + " gy, "
+                 + String(values.gz) + " gz");
 }
 
 
 void loop() {
-	gyro_ptr->read();
+  readGyro();
 }
