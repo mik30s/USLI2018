750d4a2a0193776c7b8731cbd6c50c1d1e9231c7
Michael Osei <mosei27@live.com>
2018-01-22 18:51:14 -0600

    Refactor android app code

--- android/app/src/main/java/com/example/mosei/control/MainActivity.java
+++ android/app/src/main/java/com/example/mosei/control/MainActivity.java
@@ -1,39 +1,48 @@
 package com.example.mosei.control;
 
-import ioio.lib.api.Uart;
-import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.util.BaseIOIOLooper;
-import ioio.lib.util.android.IOIOActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Checkable;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
-
-import org.json.JSONException;
+import ioio.lib.api.Uart;
+import android.os.Bundle;
 import org.json.JSONObject;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import ioio.lib.util.BaseIOIOLooper;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import ioio.lib.util.android.IOIOActivity;
+import ioio.lib.api.exception.ConnectionLostException;
+
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends IOIOActivity {
-    private ToggleButton driveRoverToggleButton;
-    private ToggleButton lockRoverToggleButton;
-    private final int ROVER_COMM_DE = 1; // unlock, orient and rover
-    private final int ROVER_COMM_DA = 2; // report data on values and sensors
-    private final int ROVER_COMM_DS = 3; // deploy solar panels.
-    private final int ROVER_COMM_DR = 4; // start rover drive.
+public class MainActivity extends IOIOActivity
+{
+    // Rover command values
+    private final int ROVER_COMM_UNLOCK = 1; // unlock, orient and rover
+    private final int ROVER_COMM_VALUES = 2; // report data on values and sensors
+    private final int ROVER_COMM_SOLAR  = 3; // deploy solar panels.
+    private final int ROVER_COMM_DRIVE  = 4; // start rover drive.
+
+    // UI references
+    private Switch lockSwitch;
+    private Switch driveSwitch;
+    private TextView driveSpeedText;
+    private SeekBar driveSpeedScroll;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        driveRoverToggleButton = findViewById(R.id.driveToggleButton);
-        lockRoverToggleButton = findViewById(R.id.lockToggleButton);
+
+        lockSwitch = findViewById(R.id.lockSwitch);
+        driveSwitch = findViewById(R.id.driveSwitch);
+        driveSpeedScroll = findViewById(R.id.driveSpeedScroll);
+        driveSpeedText = findViewById(R.id.driveSpeedText);
     }
 
     class Looper extends BaseIOIOLooper
@@ -44,11 +53,19 @@ public class MainActivity extends IOIOActivity {
         private final int IOIO_RX_PIN = 35;
         private final int IOIO_TX_PIN = 34;
         private final static int BAUD_RATE = 57600;
-        private final String ROVER_DATA_ERROR = "ROVER_DATA_ERROR";
+
+        void sendControlValues(String jsonString) {
+            try {
+                JSONObject cmdObject = new JSONObject(jsonString);
+                Log.i("Sending json Event", cmdObject.toString());
+                ostream.write(cmdObject.toString().getBytes());
+            } catch(Exception ex){
+                Log.e(TAG, ex.getMessage());
+            }
+        }
 
         @Override
-        protected void setup() throws ConnectionLostException
-        {
+        protected void setup() throws ConnectionLostException {
             // initialize IOIO board uart.
             uart = ioio_.openUart(IOIO_RX_PIN,
                                   IOIO_TX_PIN,
@@ -60,84 +77,59 @@ public class MainActivity extends IOIOActivity {
             istream = uart.getInputStream();
             ostream = uart.getOutputStream();
 
-            // add listener for rover deployment
-            driveRoverToggleButton.setOnCheckedChangeListener(
-                    new CompoundButton.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged
-                        (CompoundButton compoundButton, boolean b) {
-                            JSONObject jsonObject = new JSONObject();
-                            try {
-                                jsonObject.put("cmd", ROVER_COMM_DR);
-                                if (b == true) {
-                                    jsonObject.put("aux", 0);
-                                } else {
-                                    jsonObject.put("aux", 1);
-                                }
-                                ostream.write((jsonObject.toString() + "\n").getBytes());
-                                Log.i(TAG, jsonObject.toString());
-                            } catch(Exception ex) {
-                                Log.e(TAG, ex.getMessage());
-                            }
-                        }
-                    });
+            lockSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    sendControlValues("{" +
+                    "\"cmd\":\"" + ROVER_COMM_UNLOCK + "\"," +
+                    "\"aux\":\"" + (b ? 1 : 0) + "\"," + "}"
+                    );
+                }
+            });
 
-            lockRoverToggleButton.setOnCheckedChangeListener(
-                    new CompoundButton.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged
-                        (CompoundButton compoundButton, boolean b) {
-                            JSONObject jsonObject = new JSONObject();
-                            try {
-                                jsonObject.put("cmd", ROVER_COMM_DE);
-                                if (b == true) {
-                                    jsonObject.put("aux", 0);
-                                } else {
-                                    jsonObject.put("aux", 1);
-                                }
-                                ostream.write((jsonObject.toString() + "\n").getBytes());
-                                Log.i(TAG, jsonObject.toString());
-                            } catch(Exception ex) {
-                                Log.e(TAG, ex.getMessage());
-                            }
-                        }
-                    });
+            driveSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    sendControlValues(
+            "{" +
+                    "\"cmd\":\"" + ROVER_COMM_DRIVE+ "\"," +
+                    "\"aux\":\"" + (b ? 1 : 0) + "\"," +
+                    "\"speed\":\"" + driveSpeedScroll.getProgress()+"\"" +"}"
+                    );
+                }
+            });
+
+            driveSpeedScroll.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    driveSpeedText.setText(i+"");
+                    sendControlValues(
+                            "{" +
+                                    "\"cmd\":\"" + ROVER_COMM_DRIVE+ "\"," +
+                                    "\"aux\":\"" + (b ? 1 : 0) + "\"," +
+                                    "\"speed\":\"" + driveSpeedScroll.getProgress()+"\"" +"}"
+                    );
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+
         }
 
         @Override
-        public void loop() throws ConnectionLostException
-        {
-            JSONObject jsonObject = new JSONObject();
-            try {
-                // ask rover for sensor and other data values
-                JSONObject commandJson = new JSONObject();
-                commandJson.put("cmd", ROVER_COMM_DA);
-                ostream.write((commandJson.toString() + "\n").getBytes());
+        public void loop() throws ConnectionLostException {
 
-                // wait a few seconds before reading response.
-                Thread.sleep(2000);
-
-                // read the data sent
-                byte[] roverData = new byte[1024];
-
-                istream.read(roverData);
-
-                String dataStr = new String(roverData);
-                Log.d("Data from rover", dataStr);
-
-                // sleep again
-                Thread.sleep(500);
-            }
-            catch(Exception ex){
-                Log.e("ERROR CONTROL APP", ex.getMessage(), ex);
-            }
         }
     }
 
 
     @Override
-    protected Looper createIOIOLooper()
-    {
+    protected Looper createIOIOLooper() {
         Log.i("Control app", "called ioio thread!");
         return new Looper();
     }
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -5,6 +5,7 @@
 #include "deps.h"
 
 using HandlerCallbackType = void(*)();
+
 #define RadioSerial Serial1
 #define ServoSerial Serial3 
 #define DebugSerial Serial
@@ -18,19 +19,22 @@ public:
   {
     WHEEL_SERVO_ONE  = 1,
     WHEEL_SERVO_TWO  = 2, 
-    LOCK_SERVO       = 3,
-    DEPLOYMENT_SERVO = 4
+    LOCK_SERVO       = 4,
+    DEPLOYMENT_SERVO = 3
   };
 
   SCServo servoCtrl;
   Gyroscope* scope;
   long gravityVectorZ;
+  bool isLocked, isDriving;
+  long driveSpeed;
 
 private:
   /**
    * Creates a rover state
    */
   RoverState()
+  :isDriving(false), isLocked(false), driveSpeed(1)
   {
     servoCtrl.pSerial = &ServoSerial;
     servoCtrl.EnableTorque(ServoID::WHEEL_SERVO_TWO, 1);
@@ -44,13 +48,6 @@ private:
   }
     
 public:  
-  void drive() {
-    servoCtrl.WriteSpe(ServoID::WHEEL_SERVO_ONE, -511);
-    delay(2000);
-    servoCtrl.WriteSpe(ServoID::WHEEL_SERVO_ONE, 511);
-    delay(2000);
-  }
-
   /**
    * Creates a new rover state to be used.
    * Adheres to the singleton pattern.
@@ -63,7 +60,136 @@ public:
     }
     return instance;
   }
+  
+  /**
+   * @brief Drives the rover.
+   */
+  void drive() {
+     static long v = 0;
+    // move rover 
+    this->gravityVectorZ = scope->read().az;
+    if (this->gravityVectorZ < 0) {
+      v = 511;
+    } else if(gravityVectorZ > 0)  {
+      v = -511;
+    }
+
+    if (isDriving) { 
+      this->servoCtrl.WriteSpe(this->ServoID::WHEEL_SERVO_ONE, v * driveSpeed);
+    } else {
+      this->servoCtrl.WriteSpe(this->ServoID::WHEEL_SERVO_ONE, 0);  
+    }
+    delay(1000);
+  }
+
+  /**
+   * @brief Unlocks the rover from
+   * Docking bay.
+   */
+  void unlock() 
+  {
+    static long v = 0; 
+    // move rover 
+    this->gravityVectorZ = scope->read().az;
+    if (this->gravityVectorZ < 0) {
+      v = 1023;
+    } else if(gravityVectorZ > 0)  {
+      v = -1023;
+    }
+    delay(500);
+    
+    // move unlocking servo
+    if(isLocked == false){
+      servoCtrl.WritePos(ServoID::LOCK_SERVO,350,1000);
+      delay(1000);
+    } else {
+      servoCtrl.WritePos(ServoID::LOCK_SERVO,0,1000);
+      delay(1000);
+    }
+  }
+
+  /**
+   * @brief Sends data to the base station via on board radio.
+   */
+  void report() {
+    scope->read();
+    int solVoltage = (float(analogRead(SolarVoltage)) / 1023) * 5;
+    int batVoltage = (float(analogRead(BatteryVoltage)) / 1023) * 7.4;
+    this->gravityVectorZ = scope->values.az;
+    const String jsonStr = String("{")
+       + "ax:"+ extendHex(String(scope->values.ax, HEX)) + ","
+       + "ay:"+ extendHex(String(scope->values.ay, HEX)) + ","
+       + "az:"+ extendHex(String(scope->values.az, HEX)) + ","
+       + "gx:"+ extendHex(String(scope->values.gx, HEX)) + ","
+       + "gy:"+ extendHex(String(scope->values.gy, HEX)) + ","
+       + "gz:"+ extendHex(String(scope->values.gz, HEX)) + ","
+       + "bv:"+ extendHex(String(batVoltage, HEX))       + ","
+       + "sv:"+ extendHex(String(solVoltage, HEX))       + "}";
+    char lenStrBuf[3];
+    RadioSerial.println(jsonStr + itoa(jsonStr.length(), lenStrBuf, 16));
+    
+    DEBUG(jsonStr);
+  }
+
+  void deploy(){}
 };
+
+/**
+ * Command codes
+ * DE = Deploy Rover, 1
+ * DA = Rover Data, 2
+ * DS = Deploy Solar Panels, 3
+ * see radioComm function for full usage.
+ */
+enum CommandCode : char { DE = 1, DA = 2, DS = 3, DR = 4};
+
+// An instance of the rover state object.
+RoverState* state = nullptr;
+
+
+/**
+ * Communicates with base station and rover.
+ * Will continously send data to base station for monitoring
+ * Will process commands for controlling the rover.
+ * No commands for manual control.
+ * cmd codes
+ * DE = Unlock Rover, 1
+ * DA = Rover Data, 2
+ * DS = deploy solar panels, 3
+ * see enum abover for definition.
+ */
+void radioComm() 
+{
+  DEBUG("Radio comm called.");
+  DynamicJsonBuffer jsonBuffer;
+  const String jsonStr(RadioSerial.readStringUntil('\n'));
+  JsonObject& object = jsonBuffer.parseObject(jsonStr);
+  String cmd = object[String("cmd")];
+  
+  if (cmd.toInt() == CommandCode::DE) 
+  {
+    // unlock rover
+    String aux = object[String("aux")];
+    DebugSerial.println("cmd: " + cmd + " aux: " + aux);
+    state->isLocked = bool(aux.toInt());
+    state->unlock();
+  } else if (cmd.toInt() == CommandCode::DA) {
+    state->report();
+  } else if (cmd.toInt() == CommandCode::DS) {
+    state->deploy();
+  } 
+  else if (cmd.toInt() == CommandCode::DR) 
+  {
+    // deploy rover
+    String aux = object[String("aux")];
+    DebugSerial.println("cmd: " + cmd + " aux: " + aux);
+    state->isDriving = bool(aux.toInt());
+    String dSpeed = object[String("speed")];
+    state->driveSpeed = dSpeed.toInt();
+ }
+}
+
+
 
 /**
  * Calls a function after a certain time (in seconds)
@@ -80,7 +206,6 @@ void millisHandler(uint64_t duration, HandlerCallbackType callback)
   }
 }
 
-RoverState* state = nullptr;
 
 void setup() {
   // Serial for debugging
@@ -89,14 +214,18 @@ void setup() {
   RadioSerial.begin(57500);
   // Serial connection to Servo TTL board
   ServoSerial.begin(1000000);
-  
+ 
   state = RoverState::getInstance();
+  // lock the servo
+  state->servoCtrl.WritePos(state->ServoID::LOCK_SERVO,0,1000);
 }
 
-void loop() {
-   // process base station commands every second
-   // move rover 
-   state->drive();
+void loop() 
+{
+  // process base station commands every second
+  millisHandler(200, radioComm);
+  // drive rover.
+  state->drive();
 }
 
 
